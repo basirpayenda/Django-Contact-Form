3cabf32eb862c0333af5c55df3fd7d39bf122476
basirpayenda <ab.basir.payenda@gmail.com>
2020-01-30 20:07:58 +0500

    Initial Commit

---
+++ SendEmail/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import contact_view, contact_success
+
+urlpatterns = [
+    path('', contact_view, name='contact'),
+    path('success/', contact_success, name='contact_success')
+]
